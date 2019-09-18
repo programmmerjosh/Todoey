6c1bfbeaad56040b7aae86765fec4709c68d3403
Joshua <programmmerjosh@gmail.com>
2019-09-18 11:10:35 +0100

    Add new item feature has been added

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,14 +10,12 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["Apples", "Oranges", "Peaches", "Mangos"]
+    var itemArray = ["Apples", "Oranges", "Peaches", "Mangos"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-
     }
-
 
     // MARK: - Table view data source
 
@@ -26,7 +24,6 @@ class ToDoListViewController: UITableViewController {
         return itemArray.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
@@ -47,49 +44,34 @@ class ToDoListViewController: UITableViewController {
         }
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    // MARK: - Add button pressed
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var myTextfield = UITextField()
+        let alert       = UIAlertController(title: "Add Item", message: "Insert your new item here:", preferredStyle: .alert)
+        let action      = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                          self.itemArray.append(myTextfield.text!)
+                          self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Example: Apples"
+            myTextfield = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
